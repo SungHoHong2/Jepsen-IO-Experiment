0d105b89c9f39ee5a47ad8265362e22fce5f222a
maverickjin88 <maverickjin88@gmail.com>
2020-07-09 13:17:39 -0700

    creating tutorial for Clojure

--- clojure/statement/core.clj
+++ clojure/statement/core.clj
@@ -42,27 +42,29 @@
         (println "Both the values are equal")
         (println "true")
     )
+    (do
+        (println "Both the values are not equal")
+        (println "false")
+     )
   )
 
-;  (do(println "Both the values are not equal")
-;      (println "false")
-;  )
-;
-;  if ( and (= 2 2) (= 3 3))
-;    (println "Values are equal")
-;    (println "Values are not equal")
-;
-;  (def x 5)
-;  (case x 5 (println "x is 5")
-;          10 (println "x is 10")
-;          (println "x is neither 5 nor 10"))
-;
-;  (def x 5)
-;  (cond
-;     (= x 5) (println "x is 5")
-;     (= x 10)(println "x is 10")
-;     :else (println "x is not defined")
-;  )
+
+  (if ( and (= 2 2) (= 3 3))
+    (println "Values are equal")
+    (println "Values are not equal")
+  )
+
+  (def x 5)
+  (case x 5 (println "x is 5")
+          10 (println "x is 10")
+          (println "x is neither 5 nor 10"))
+
+  (def x 5)
+  (cond
+     (= x 5) (println "x is 5")
+     (= x 10)(println "x is 10")
+     :else (println "x is not defined")
+  )
 
 
 )
