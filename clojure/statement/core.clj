898ac2533fb6c5385e0a077f460afb7f44c481d0
maverickjin88 <maverickjin88@gmail.com>
2020-07-09 11:50:11 -0700

    creating tutorial for Clojure

--- clojure/statement/core.clj
+++ clojure/statement/core.clj
@@ -24,15 +24,42 @@
     (println n)
   )
 
-
   ; loop
-  (println "Looping")
   (loop [x 10]
     (when (> x 1)
         (println x)
         (recur (- x 2))
     )
   )
+
+  if ( = 2 2)
+    (println "Values are equal")
+    (println "Values are not equal")
+
+  if (= 2 2)
+  (do(println "Both the values are equal")
+      (println "true")
+  )
+  (do(println "Both the values are not equal")
+      (println "false")
+  )
+
+  if ( and (= 2 2) (= 3 3))
+    (println "Values are equal")
+    (println "Values are not equal")
+
+  (def x 5)
+  (case x 5 (println "x is 5")
+          10 (println "x is 10")
+          (println "x is neither 5 nor 10"))
+
+  (def x 5)
+  (cond
+     (= x 5) (println "x is 5")
+     (= x 10)(println "x is 10")
+     :else (println "x is not defined")
+  )
+
 
 )
 
