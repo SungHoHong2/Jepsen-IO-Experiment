9b607029cf9fc08f4e02656dddbc0ca9c0989807
maverickjin88 <maverickjin88@gmail.com>
2020-07-09 15:03:53 -0700

    creating tutorial for Clojure

--- clojure/structure/core.clj
+++ clojure/structure/core.clj
@@ -8,10 +8,11 @@
   (def emp (struct Employee "John" 1))
   (println emp)
 
-  (def emp2 (struct-map Employee :EmployeeName "John" :Employeeid 1))
+  (def emp2 (struct-map Employee :EmployeeName "Chris" :Employeeid 2))
   (println emp2)
 
-
+  (println (:Employeeid emp))
+  (println (:EmployeeName emp))
 
 )
 
