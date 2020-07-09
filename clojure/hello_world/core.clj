5bb599beda67180d34818211dd322bb2f81b9061
maverickjin88 <maverickjin88@gmail.com>
2020-07-09 11:32:35 -0700

    creating tutorial for Clojure

--- clojure/hello_world/core.clj
+++ clojure/hello_world/core.clj
@@ -3,11 +3,23 @@
 
 
 (defn -main [& args]
+
   (println "Hello, Sungho!")
   (println (+ 1 2))
   (println (str "Hello World"))
   (println (+ 1 2 3))
   (println [+ 1 2 3]))
+
+  ;; The below code declares a integer variable
+  (def x 1)
+  ;; The below code declares a float variable
+  (def y 1.25)
+  ;; The below code declares a string variable
+  (def str1 "Hello")
+  (println x)
+  (println y)
+  (println str1)
+
 
 (-main)
 
