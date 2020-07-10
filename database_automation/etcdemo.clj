f2f55d99b0c0c27635e82344132306a5e90fe454
maverickjin88 <maverickjin88@gmail.com>
2020-07-09 17:42:59 -0700

    testing Jepsen tutorials

--- database_automation/etcdemo.clj
+++ database_automation/etcdemo.clj
@@ -49,20 +49,19 @@
                                  "/etcd-" version "-linux-amd64.tar.gz")]
                     (cu/install-archive! url dir))
 
-;                  (cu/start-daemon!
-;                   {:logfile logfile
-;                    :pidfile pidfile
-;                    :chdir   dir}
-;                   binary
-;                   :--log-output                   :stderr
-;                   :--name                         (name node)
-;                   :--listen-peer-urls             (peer-url   node)
-;                   :--listen-client-urls           (client-url node)
-;                   :--advertise-client-urls        (client-url node)
-;                   :--initial-cluster-state        :new
-;                   :--initial-advertise-peer-urls  (peer-url node)
-;                   :--initial-cluster              (initial-cluster test))
-
+                  (cu/start-daemon!
+                   {:logfile logfile
+                    :pidfile pidfile
+                    :chdir   dir}
+                   binary
+                   :--log-output                   :stderr
+                   :--name                         (name node)
+                   :--listen-peer-urls             (peer-url   node)
+                   :--listen-client-urls           (client-url node)
+                   :--advertise-client-urls        (client-url node)
+                   :--initial-cluster-state        :new
+                   :--initial-advertise-peer-urls  (peer-url node)
+                   :--initial-cluster              (initial-cluster test))
 
 
 
