0c71d849b28ed19ac3ee44afdd254d1fc3c8b8c4
maverickjin88 <maverickjin88@gmail.com>
2020-07-17 11:53:43 -0700

    updating with comments

--- scaffolding/etcdemo.clj
+++ scaffolding/etcdemo.clj
@@ -3,13 +3,16 @@
               [jepsen.tests :as tests]))
 
 
+; the function that runs the etcd test
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
+  ; function is extended with the noop-test
   (merge tests/noop-test
          opts))
 
+; main function
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
