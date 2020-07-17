165ce1a322fc13750ab04e121e35bf0835026268
maverickjin88 <maverickjin88@gmail.com>
2020-07-17 13:24:52 -0700

    updating with comments

--- database/etcdemo.clj
+++ database/etcdemo.clj
@@ -13,6 +13,7 @@
 (def logfile (str dir "/etcd.log"))
 (def pidfile (str dir "/etcd.pid"))
 
+; a collection of functions used for installing the etcd
 (defn node-url
   "An HTTP url for connecting to a node on a particular port."
   [node port]
@@ -42,13 +43,17 @@
   "Etcd DB for a particular version."
   [version]
   (reify db/DB
+         ; follows the installation procedures of etcd
          (setup! [_ test node]
                  (info node "installing etcd" version)
+                 ; uses the sudo command
                  (c/su
+                  ; download the tar file from the website
                   (let [url (str "https://storage.googleapis.com/etcd/" version
                                  "/etcd-" version "-linux-amd64.tar.gz")]
                     (cu/install-archive! url dir))
 
+                  ; follows the clustering procedures by invoking each functions
                   (cu/start-daemon!
                    {:logfile logfile
                     :pidfile pidfile
@@ -64,11 +69,13 @@
                    :--initial-cluster              (initial-cluster test))
                   ))
 
+         ; stops the function and removes the etcd
          (teardown! [_ test node]
                     (info node "tearing down etcd")
                     (cu/stop-daemon! binary pidfile)
                     (c/su (c/exec :rm :-rf dir)))
 
+         ; store the log files
          db/LogFiles
          (log-files [_ test node]
                     [logfile])
