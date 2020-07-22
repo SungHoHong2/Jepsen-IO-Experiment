09db0ea5e7047686d315ce24122f8f008076649a
maverickjin88 <maverickjin88@gmail.com>
2020-07-22 10:15:19 -0700

    testing elle

--- elle/etcdemo.clj
+++ elle/etcdemo.clj
@@ -1,6 +1,5 @@
 ; namespace and the included external APIs
 (ns jepsen.etcdemo
-;    (:gen-class)
     (:require [jepsen.cli :as cli]
               ; jepsen.tests contains a noop-test that runs by the workers
               [jepsen.tests :as tests]))
