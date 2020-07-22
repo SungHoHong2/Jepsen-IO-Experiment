b68752bbae22789a0f0c4ee2c710b1c6aacb5de1
maverickjin88 <maverickjin88@gmail.com>
2020-07-22 10:22:42 -0700

    testing elle

--- elle/etcdemo.clj
+++ elle/etcdemo.clj
@@ -1,16 +1,12 @@
 ; namespace and the included external APIs
 (ns jepsen.etcdemo
-    (:require [clojure [test :refer :all]
+    (:require [jepsen.cli :as cli]
+              [jepsen.tests :as tests]
+              [clojure [test :refer :all]
                [edn :as edn]]
-              [clojure.java.io :as io]
-              [dom-top.core :refer [real-pmap]]
-              [elle [core :refer :all]
-               [graph :as g]]
-              [jepsen.txn :as txn]
-              [knossos [history :as history]
-               [op :as op]]
-              [slingshot.slingshot :refer [try+ throw+]])
-    (:import (java.io PushbackReader)))
+
+              ))
+
 
 
 ; main function
