b269b92145e33fe564a6baece3af78d5c2265532
maverickjin88 <maverickjin88@gmail.com>
2020-07-23 13:43:15 -0700

    clojure file example

--- clojure/file/core.clj
+++ clojure/file/core.clj
@@ -8,6 +8,9 @@
   (spit "example.txt"
         "This is a string")
 
+  (with-open [w (clojure.java.io/writer "example.txt" :append true)]
+    (.write w (str "hello" "world")))
+
   (def string1 (slurp "example.txt"))
   (println string1)
 
