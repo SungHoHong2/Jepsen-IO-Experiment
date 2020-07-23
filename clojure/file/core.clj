f59197f819e5637d3dad11b67e2657f651b11b0f
maverickjin88 <maverickjin88@gmail.com>
2020-07-23 13:42:36 -0700

    clojure file example

--- clojure/file/core.clj
+++ clojure/file/core.clj
@@ -6,15 +6,10 @@
 
 
   (spit "example.txt"
-        "This is a string"))
+        "This is a string")
 
   (def string1 (slurp "example.txt"))
   (println string1)
-
-  (with-open [rdr (clojure.java.io/reader "example.txt")]
-    (reduce conj [] ( line-seq rdr)))
-
-
 
 
 
