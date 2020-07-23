684a8c153af9e2768bd9caf8c2eafe1849749400
maverickjin88 <maverickjin88@gmail.com>
2020-07-23 13:45:51 -0700

    clojure file example

--- clojure/file/core.clj
+++ clojure/file/core.clj
@@ -4,9 +4,7 @@
 
 (defn -main [& args]
 
-
-  (spit "example.txt"
-        "First Sentence\n")
+  (spit "example.txt" "First Sentence\n")
 
   (with-open [w (clojure.java.io/writer "example.txt" :append true)]
     (.write w (str "Second " "Sentence")))
