fdc602007bff237ae3a835289cac7cb1e3609d7a
maverickjin88 <maverickjin88@gmail.com>
2020-07-24 12:33:36 -0700

    jepsen test

--- elle/redis/core.clj
+++ elle/redis/core.clj
@@ -180,7 +180,7 @@
     (->> (for [i counts, n nemeses, w workloads]
            (assoc opts :nemesis n :workload w))
 
-         (doseq [[k v] opts] (println "\t" k v))
+;         (doseq [[k v] opts] (println "\t" k v))
          (map redis-test)
     )
   )
