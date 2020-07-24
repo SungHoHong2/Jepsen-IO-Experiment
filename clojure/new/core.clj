98b4b18c8f7693982ddd566a20cac0e5b9227cf7
maverickjin88 <maverickjin88@gmail.com>
2020-07-24 11:26:20 -0700

    jepsen test

--- clojure/new/core.clj
+++ clojure/new/core.clj
@@ -6,13 +6,12 @@
   (println "assoc")
   (def test1 {})
   (assoc test1 :key1 "value" :key2 "another value")
-  (println test1)
-
+  (println "assoc empty hashmap: " test1)
 
   (def test2 {:account-no 12345678 :lname "Jones" :fnam "Fred"})
   (def modified (assoc test2 :fnam "Sue"))
-  (println test2)
-  (println modified)
+  (println "before assoc" test2)
+  (println "after assoc" modified)
 
 
 )
