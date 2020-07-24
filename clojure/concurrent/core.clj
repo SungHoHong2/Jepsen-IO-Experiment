22d272877df1cf58ad6e5e21ce766882ff0f56fd
maverickjin88 <maverickjin88@gmail.com>
2020-07-23 17:22:13 -0700

    clojure concurrent

--- clojure/concurrent/core.clj
+++ clojure/concurrent/core.clj
@@ -15,6 +15,18 @@
   (println @names)
 
 
+  (def var1 (ref 10))
+  (def var2 (ref 20))
+  (println @var1 @var2)
+
+  (defn change-value [var1 var2 newvalue]
+    (dosync
+     (alter var1 - newvalue)
+     (alter var2 + newvalue)))
+
+
+  (change-value var1 var2 20)
+  (println @var1 @var2)
 
 
 )
