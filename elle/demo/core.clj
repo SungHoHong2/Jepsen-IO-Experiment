00c04ef510605b2dc39f5c6bcf0a0fca01e649a7
maverickjin88 <maverickjin88@gmail.com>
2020-07-30 09:36:59 -0700

    update Jepsen Elle

--- elle/demo/core.clj
+++ elle/demo/core.clj
@@ -1,6 +1,8 @@
 (ns jepsen.redis.core
     "Top-level test runner, integration point for various workloads and nemeses."
-    (:require [elle.list-append :as a]))
+    (:require [elle.list-append :as a]
+              [clojure [pprint :refer [pprint]]
+               [string :as str]]))
 
 
 (defn -main
@@ -8,7 +10,11 @@
 
   (println "BEGIN Elle")
 
+  (def h [{:type :ok, :value [[:append :x 1] [:r :y [1]]]}
+          {:type :ok, :value [[:append :x 2] [:append :y 1]]}
+          {:type :ok, :value [[:r :x [1 2]]]}])
 
+  (pprint (a/check {:consistency-models [:serializable], :directory "out"} h))
 
   (println "END Elle")
 
