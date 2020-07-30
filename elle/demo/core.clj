744002af6462d53399f3ae478ea33e18903f19b9
maverickjin88 <maverickjin88@gmail.com>
2020-07-30 11:50:36 -0700

    update

--- elle/demo/core.clj
+++ elle/demo/core.clj
@@ -4,42 +4,39 @@
               [clojure [pprint :refer [pprint]]
                [string :as str]]))
 
-;(ns jepsen.redis.core
-;    (:refer-clojure :exclude [test])
-;    (:require [clojure [pprint :refer [pprint]]
-;               [test :refer :all]]
-;              [elle [core :as elle]
-;               [core-test :refer [read-history]]
-;               [list-append :refer :all]
-;               [graph :as g]
-;               [util :refer [map-vals]]]
-;              [jepsen.txn :as txn]
-;              [knossos [history :as history]
-;               [op :as op]]
-;              [slingshot.slingshot :refer [try+ throw+]]))
-;
-
-
 (defn -main
   [& args]
 
-  (println "G0")
-    (def G0 [  {:type :ok, :value [[:append :x 1] [:append :y 1]]}
-               {:type :ok, :value [[:append :x 2] [:append :y 2]]}
-               {:type :ok, :value [[:r :x [1 2]] [:r :y [2 1]]]}     ])
+    ; G0: A write cycle == (P0) Dirty Write
+    ;
+    ; T1: W(x1)                   W(y1) c
+    ; T2:       W(x2)   W(y2) c
+    ; -----------------------------------
+    ; T3: R(x1)(y2)  R(x2)(y1)
 
-;  [{:type :ok, :value [[:append :x 2] [:append :y 2]]}
-;   {:type :ok, :value [[:append :x 1] [:append :y 1]]}
-;   {:type :ok, :value [[:append :x 2] [:append :y 2]]}]
+    (def G0 [
+              ; transaction 1
+              {:type :ok, :value [[:append :x 1] [:append :y 1]]}
+              ; transaction 2
+              {:type :ok, :value [[:append :x 2] [:append :y 2]]}
+              ; transaction 3: reads updated x and y from both T1 and T2
+              {:type :ok, :value [[:r :x [1 2]] [:r :y [2 1]]]}])
 
+    ; assuming that the database is serializable and returns the anomaly to directory out
     (pprint (a/check {:consistency-models [:serializable], :directory "out"} G0))
 
 
+    ; G1a: Aborted Read: T2 sees T1's failed write
 
 
+    (def G1a [
+              ; transaction 1
+              {:type :fail, :value [[:append :x 1]]}
+              ; transaction 2: reads the aborted x from T1
+              {:type :ok, :value [[:r :x [1]]]}])
 
+    (pprint (a/check {:consistency-models [:serializable], :directory "out"} G1a))
 
-  ; G1a: Aborted Read
 
   ; G1b: Intermediate Reads
 
