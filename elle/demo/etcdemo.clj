f9a3ce6caca64d16e811222b10f961bb7a03dbac
maverickjin88 <maverickjin88@gmail.com>
2020-07-30 09:16:55 -0700

    update Jepsen Elle

--- elle/demo/etcdemo.clj
+++ elle/demo/etcdemo.clj
@@ -1,24 +1,16 @@
 ; namespace and the included external APIs
 (ns jepsen.etcdemo
     ; jepsen.cli creates the workers
-    (:require [jepsen.cli :as cli]
-    ; jepsen.tests contains a noop-test that runs by the workers
-              [jepsen.tests :as tests]))
-
-; the function that runs the etcd test
-(defn etcd-test
-  "Given an options map from the command line runner (e.g. :nodes, :ssh,
-  :concurrency, ...), constructs a test map."
-  [opts]
-  ; function is extended with the noop-test
-  ; noop-test does nothing
-  (merge tests/noop-test
-         opts))
+    (:require [elle.list-append :as a]))
 
 ; main function
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
-            args))
+
+  (println "Hello World")
+
+
+
+)
