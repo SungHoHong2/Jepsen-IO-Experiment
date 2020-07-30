4b8101a414ad8dc5f20370e5d8e2ec28817efaa7
maverickjin88 <maverickjin88@gmail.com>
2020-07-30 11:30:20 -0700

    update

--- elle/demo/core.clj
+++ elle/demo/core.clj
@@ -26,7 +26,7 @@
   (println "G0")
     (def G0 [  {:type :ok, :value [[:append :x 1] [:append :y 1]]}
                {:type :ok, :value [[:append :x 2] [:append :y 2]]}
-               {:type :ok, :value [[:r :x [1 2]] [:r :x [1 2]]]}     ])
+               {:type :ok, :value [[:r :x [1 2]] [:r :y [2 1]]]}     ])
 
 ;  [{:type :ok, :value [[:append :x 2] [:append :y 2]]}
 ;   {:type :ok, :value [[:append :x 1] [:append :y 1]]}
