b8477b7eb4c9b9066fda487fc86c567085d9d4bd
maverickjin88 <maverickjin88@gmail.com>
2020-07-31 16:57:08 -0700

    jepsen lost update test

--- elle/demo/core.clj
+++ elle/demo/core.clj
@@ -14,13 +14,11 @@
 
       (let [testcase (first args)]
         (case testcase
-              "G0" (
-                     (println "Hello?")
-                   )
+              "G0" 0
               "shark" (println "FRISK" (count mystr))))
 
 
-    ; G0: A write cycle == (P0) Dirty Write
+;    ; G0: A write cycle == (P0) Dirty Write
 ;    (def G0 [
 ;              ; transaction 1
 ;              {:type :ok, :value [[:append :x 1] [:append :y 1]]} ; x[1] y[1]
@@ -29,9 +27,9 @@
 ;              ; transaction 3: reads updated x and y from both T1 and T2
 ;              {:type :ok, :value [[:r :x [1 2]] [:r :y [2 1]]]}
 ;            ])
-
+;
 ;    (pprint (a/check {:consistency-models [:serializable], :directory "out"} G0))
-
+;
 ;    ; G1a: Aborted Read, T2 sees T1's failed write
 ;    (def G1a [
 ;              ; transaction 1
