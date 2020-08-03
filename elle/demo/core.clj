d125eccc8374d86b5d7ee1a54a175bf4a4f312b7
maverickjin88 <maverickjin88@gmail.com>
2020-08-03 13:45:33 -0700

    update testSample

--- elle/demo/core.clj
+++ elle/demo/core.clj
@@ -17,9 +17,10 @@
               ; transaction 1
               {:type :ok, :value [[:append :x 1] [:append :y 1]]} ; x[1] y[1]
               ; transaction 2
-              {:type :ok, :value [[:append :x 2] [:append :y 2]]} ; x[1,2] y[1,2] or x[2,1] y[2,1]
+              {:type :ok, :value [[:append :x 2] [:append :y 2]]}
               ; transaction 3: reads updated x and y from both T1 and T2
               {:type :ok, :value [[:r :x [1 2]] [:r :y [2 1]]]}
+              ; transaction 3 should have x[1,2] y[1,2] or x[2,1] y[2,1]
             ])
 
     ; G1a: Aborted Read, T2 sees T1's failed write
@@ -28,14 +29,16 @@
               {:type :fail, :value [[:append :x 1]]}
               ; transaction 2: reads the aborted x from T1
               {:type :ok, :value [[:r :x [1]]]}
+              ; transaction 2 should be aborted
              ])
 
-    ; G1b: Intermediate Reads, T2 sees T1's intermediate write
+    ; G1b: Intermediate Reads, T2 sees T1's intermediate write == Fuzzy Read
     (def G1b [
               ; transaction 1
               {:type :ok, :value [[:append :x 1] [:append :x 2]]}
               ; transaction 2
               {:type :ok, :value [[:r :x [1]]]}
+              ; transaction 2 should have x[1,2]
              ])
 
     ; G1c: Circular Information Flow, T2 writes x after T1, but T1 observes T2's write on y.
@@ -46,6 +49,7 @@
               {:type :ok, :value [[:append :x 2] [:append :y 1]]}
               ; transaction 3: T1 -> T2 but T1 seen T2's write
               {:type :ok, :value [[:r :x [1 2]] [:r :y [1]]]}
+              ; either transaction 1 or 2 have to be aborted
               ])
 
     ; G2: Anti-dependency Cycles
@@ -56,14 +60,15 @@
                {:type :ok, :value [[:append :y 1] [:r :x]]}
                ])
 
-    ; incompatible-order == LostUpdate
+    ; incompatible-order == Lost Update
     (def LostUpdate [
                ; transaction 1
                {:type :ok, :value [[:append :x 1] [:r :x [1]]]}
                ; transaction 2
                {:type :ok, :value [[:append :x 2] [:r :x [1 2]]]}
                ; transaction 3: T1 update is lost
-               {:type :ok, :value [[:r :x [2]]]} ; should expect [1,2] or [2,1]
+               {:type :ok, :value [[:r :x [2]]]}
+               ; transaction 3 should have [1,2]
                ])
 
     (let [testcase (first args)]
